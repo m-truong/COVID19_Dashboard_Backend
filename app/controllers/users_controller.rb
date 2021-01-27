4b33b0601bbfbd92473a20c7a34b8441d2573060
Michael Truong <michaeltruong@Michaels-MacBook-Pro.local>
2021-01-27 15:37:46 -0500

    added create_token method and payload method to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,11 +49,26 @@ end
   end
 
   private
+    # Method to create token with payload
+    def create_token(id, username)
+      JWT.encode(payload(id, username), ENV['JWT_SECRET'], 'HS256')
+    end
+    # Method to return a hash
+    def payload(id, username)
+      {
+        exp: (Time.now + 30.minutes).to_i,
+        iat: Time.now.to_i,
+        iss: ENV['JWT_ISSUER'],
+        user: {
+          id: id,
+          username: username
+        }
+      }
+    end
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
     end
-
     # Only allow a list of trusted parameters through.
     def user_params
       params.require(:user).permit(:username, :password_digest)
